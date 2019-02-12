7b325be95d878ab04333e014b1237f4d32d16434
kubitre <kubitre@gmail.com>
2019-02-13 06:23:36 +0700

    [UPDATE] Update parser for configuration and create comment

--- Config/configuration.go
+++ Config/configuration.go
@@ -1,7 +1,19 @@
 package Config
 
+import (
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
 /*Configuration - it is struct for parsing settings from .toml file*/
 type Configuration struct {
 	Server   string
 	Database string
+}
+
+func (c *Configuration) Read() {
+	if _, err := toml.DecodeFile("config.toml", &c); err != nil {
+		log.Fatal(err)
+	}
 }
