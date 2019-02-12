4a9024603b70f449619f0e552fa8cdc09d25eb42
kubitre <kubitre@gmail.com>
2019-02-13 06:24:49 +0700

    [UPDATE] Fix comments for model

--- Models/user.go
+++ Models/user.go
@@ -6,10 +6,11 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+/*User - it is structure for main type user in our system*/
 type User struct {
 	ID          bson.ObjectId `bson:"_id" json:"id"`
 	Username    string        `bson:"name" json:"username"`
-	Password    string        `bson:"password" json:""`
+	Password    string        `bson:"password" json:"password"`
 	Email       string        `bson:"email" json:"email"`
 	Verificated bool          `bson:"verificated" json:"verificated"`
 	CreatedAt   time.Time     `bson:"createdat" json:"createdat"`
