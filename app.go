1565ea68a398ef8081eb651de9f10a59a9949bcd
kubitre <kubitre@gmail.com>
2019-02-13 06:23:51 +0700

    [ADD] Create init func

--- app.go
+++ app.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kubitre/blog/Dao"
+
+	"github.com/kubitre/blog/Config"
+	"github.com/kubitre/blog/Routes"
+)
+
+func init() {
+	var conf = Config.Configuration{}
+	conf.Read()
+
+	var daoServer = Dao.SettingDao{Server: conf.Server, Database: conf.Database}
+	daoServer.Connect()
+
+	Routes.StartSettingRoutes(conf)
+	fmt.Println("api was started")
+}
 
 func main() {
-	fmt.Println("Start api")
+	fmt.Println("Api was started")
 }
