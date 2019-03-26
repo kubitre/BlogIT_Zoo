5fc226ffbb0286a66ca01ae76a4c4e5994219989
kubitre <kubitre@gmail.com>
2019-03-26 15:52:44 +0700

    [UPD] update middleware checker for token validation by signing method ecdsa

--- Middlewares/token_check.go
+++ Middlewares/token_check.go
@@ -6,27 +6,16 @@ import (
 	"net/http"
 	"strings"
 
-	jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/kubitre/blog/security"
+
 	"github.com/kubitre/blog/Dao"
 )
 
 /*JWTChecker - прослойка для проверки токена*/
 type JWTChecker struct {
-	jwtMiddleWare jwtmiddleware.JWTMiddleware
+	JWTMiddleWare *security.ECDSAMiddle
 	DaoToken      *Dao.TokenDao
-}
-
-/*CheckerLayerConfiguration - прослойка с проверкой токена*/
-func (tjwt *JWTChecker) CheckerLayerConfiguration(signKey string) jwtmiddleware.JWTMiddleware {
-	tjwt.jwtMiddleWare = *jwtmiddleware.New(jwtmiddleware.Options{
-		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			return []byte(signKey), nil
-		},
-		SigningMethod: jwt.SigningMethodHS256,
-	})
-
-	return tjwt.jwtMiddleWare
+	Responser     *Responser
 }
 
 /*JWTCMiddleware - прослойка проверки токена*/
@@ -36,19 +25,41 @@ func (tjwt *JWTChecker) JWTCMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		tokenHeader := r.Header.Get("Authorization")
 		tokenValue := strings.Split(tokenHeader, " ")[1]
 
-		log.Println("Start authentification check, auth token: ", tokenHeader)
+		// log.Println("Start authentification check, auth token: ", tokenHeader)
 
 		fmt.Println("Token value: ", tokenValue)
 
 		// 1 STEP: FInd Token in DAO Token layer
 		tokenFromDb, err := tjwt.DaoToken.FindTokenByValue(tokenValue)
+
 		if err != nil {
 			fmt.Println("Has error find token in database. Error code: ", err.Error())
+			tjwt.Responser.ResponseWithError(w, r, http.StatusUnauthorized, map[string]string{
+				"error": err.Error(),
+				"trace": "jwtmidleware_checker",
+			})
+
+			return
 		}
 		fmt.Println("Token from db: ", tokenFromDb)
 
-		// 2 STEP: Checking validatioon of enter token
+		// 2 STEP: validation duration time and created time of token
+
+		// 3 STEP: Checking validatioon of enter token
+		result, err := tjwt.JWTMiddleWare.TokenValidation(tokenValue)
+		if err != nil && !result {
+			tjwt.Responser.ResponseWithError(w, r, http.StatusUnauthorized, map[string]string{
+				"error": err.Error(),
+			})
+		}
+
+		log.Println("Authorized completed!")
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+/*ConfiguratingJWTWithSigningMethod - конфигурация мидлвари на чек токена*/
+func (tjwt *JWTChecker) ConfiguratingJWTWithSigningMethod(jwtmiddleware *security.ECDSAMiddle) {
+	tjwt.JWTMiddleWare = jwtmiddleware
 }
